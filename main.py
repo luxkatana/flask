4ae8c73e71686db35867fb2cf0ff0b179d6b66b6
luxkatana <taseen.bibi@gmail.com>
2022-07-28 21:59:42 +0200

    First commit

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from flask import Flask, jsonify
+from flask_restful import Api, Resource
+
 import os
 
 app = Flask(__name__)
-
-
+api = Api(app)
+class something(Resource):
+    def get(self):
+        return jsonify({"message": "roblox, is that you?"})
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    return jsonify({"Message": "Hello, World?"})
 
+api.add_resource(something, "/")
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
