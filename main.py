6a7aca9dc0066058e100de837b1a9e11105ce544
unknown <taseen.bibi@gmail.com>
2022-07-29 23:37:47 +0200

    w

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-
+from random import randint
 app = Flask(__name__)
 
 @app.route("/")
@@ -10,7 +10,7 @@ def hello_world():
 def test_api():
     if request.method == "POST":
         print("The data is: {}".format(request.data.decode()))
-        return jsonify({"status": "printed data"})
+        return jsonify({"randNum": randint(1, 100)})
 
 if __name__ == "__main__":
     app.run(debug=True, port=80)
