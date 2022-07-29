b605e63d581b9598e305487a292c1106f13f6612
unknown <taseen.bibi@gmail.com>
2022-07-29 23:10:36 +0200

    Added data parm

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 def hello_world():
     return "<h> Zuyo Hub SS winning?</h>"
 
-@app.route("/api", methods=["GET"])
+@app.route("/api", methods=["POST"])
+def test_api(data):
+    if request.method == "POST":
+        print(data)
+        return jsonify({"status": "successfully printed data!"})
 
-def test_api():
-    if request.method == "GET":
-        print("OOF FOUND SOME KID HACKING ON FORTNITE XD")
-        return jsonify({"message": "succcess kiddo"})
 if __name__ == "__main__":
     app.run(debug=True, port=80)
