699fd9a0d8323bca02d858d0bb7c16a242e13c42
unknown <taseen.bibi@gmail.com>
2022-07-29 23:22:48 +0200

    e

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def test_api():
     if request.method == "POST":
         print(request.data)
 
-        return jsonify({"status": "successfully printed data!"})
+        return jsonify({"data responded:": request.data["data"]})
 
 if __name__ == "__main__":
     app.run(debug=True, port=80)
