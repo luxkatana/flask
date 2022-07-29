2ac2794fe3bc94cef6978ba5da223b6b2340c03f
unknown <taseen.bibi@gmail.com>
2022-07-29 23:02:14 +0200

    Edit from windows 11 pro

--- main.py
+++ main.py
@@ -1,33 +1,16 @@
-from flask import Flask, jsonify
-from flask_restful import Api, Resource
-import psycopg2
-import os
-import dotenv
+from flask import Flask, jsonify, request
+
 app = Flask(__name__)
-api = Api(app)
-dotenv.load_dotenv()
-DBHOST = os.environ["DBHOST"]
-DBNAME = os.environ["DBNAME"]
-DBUSER = os.environ["DBUSER"]
-DBPASSWORD = os.environ["DBPASSWORD"]
-DBPORT = os.environ["DBPORT"]
-connection = psycopg2.connect(database=DBNAME, user=DBUSER, password=DBPASSWORD, host=DBHOST, port=DBPORT)
-cursor = connection.cursor()
-class something(Resource):
-    def get(self, data):
-        try:
-            data["robloxID"]
-        except KeyError as e:
-            return jsonify({"error": "missing robloxID"})
-        cursor.execute("select * from whitelisted where robloxID=?;", (data["robloxID"],))
-        d = cursor.fetchall()
-        return jsonify({"status": d[0]["robloxid"]})
 
-@app.route('/')
-def index():
-    return jsonify({"Message": "Hello, World?"})
+@app.route("/")
+def hello_world():
+    return "<h> Zuyo Hub SS winning?</h>"
 
-api.add_resource(something, "/")
+@app.route("/api", methods=["GET"])
 
-if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+def test_api():
+    if request.method == "GET":
+        print("OOF FOUND SOME KID HACKING ON FORTNITE XD")
+        return jsonify({"message": "succcess kiddo"})
+if __name__ == "__main__":
+    app.run(debug=True, port=80)
