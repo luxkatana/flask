111aca31750a05bb57434f43a3869f38ef6f233b
unknown <taseen.bibi@gmail.com>
2022-08-01 18:27:05 +0200

    End

---
+++ clickthisFile.bat
@@ -0,0 +1,9 @@
+echo Installing the modules what is needed to run the FLASK app
+echo this zip/folder was meant to host and communicate with the roblox serverside zuyoSERVERSIDE
+echo created by luxkatana/TheTrojanHorse
+
+pip3 install -r requirements.txt
+echo done with installing the packages
+
+echo DM me back when u are done
+
--- testreq.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-get = requests.get("http://127.0.0.1/api", params={"robloxID": 800926308})
-
-print(get.json())
